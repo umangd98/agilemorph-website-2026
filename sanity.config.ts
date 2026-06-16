f3dc503851b1acb7131650de955285d244516272
satvik2131 <satvik213161@gmail.com>
2026-06-16 18:12:53 +0530

    added configs

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,8 @@ import { structureTool } from "sanity/structure";
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
 import { schemaTypes } from "./src/sanity/schemaTypes";
 
+const singletonTypes = new Set(["homepage", "aboutPage", "contactPage"]);
+
 export default defineConfig({
   name: "agilemorph-site",
   title: "AgileMorph",
@@ -15,6 +17,8 @@ export default defineConfig({
   dataset,
   schema: {
     types: schemaTypes,
+    templates: (templates) =>
+      templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
   },
   plugins: [
     structureTool({
@@ -31,9 +35,27 @@ export default defineConfig({
                   .documentId("homepage")
                   .title("Homepage"),
               ),
-            ...S.documentTypeListItems().filter(
-              (listItem) => !["homepage"].includes(listItem.getId() ?? ""),
-            ),
+            S.listItem()
+              .title("About Page")
+              .id("aboutPage")
+              .child(
+                S.document()
+                  .schemaType("aboutPage")
+                  .documentId("aboutPage")
+                  .title("About Page"),
+              ),
+            S.listItem()
+              .title("Contact Page")
+              .id("contactPage")
+              .child(
+                S.document()
+                  .schemaType("contactPage")
+                  .documentId("contactPage")
+                  .title("Contact Page"),
+              ),
+            S.divider(),
+            S.documentTypeListItem("servicePage").title("Service Pages"),
+            S.documentTypeListItem("blogPost").title("Blog Posts"),
           ]),
     }),
     visionTool({ defaultApiVersion: apiVersion }),
